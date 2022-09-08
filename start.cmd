1b5468a87421443715f0d2357b525009644bc75c
astrohnugget <112528978+astrohnugget@users.noreply.github.com>
2022-09-08 17:14:09 -0500

    update2.0

--- start.cmd
+++ start.cmd
@@ -1,20 +1,5 @@
-@ECHO OFF
-
-:choice
-set /P c=Do you want hamburgers?[Y/N]?
-if /I "%c%" EQU "Y" goto :somewhere
-if /I "%c%" EQU "N" goto :somewhere_else
-goto :choice
-
-
-:somewhere
 
 :1
 start hamburger.vbs
 goto 1
 
-:somewhere_else
-
-echo "ok, goodbye!"
-pause
-exit
